35792af6d883b497162d21d55dc077c7fc2ebb34
Oskar <oskarpasko@gmail.com>
2024-11-24 19:44:36 +0100

    create backend project

---
+++ panda-quiz/src/main/java/com/example/panda_quiz/PandaQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.panda_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaQuizApplication.class, args);
+	}
+
+}
